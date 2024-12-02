285a28398fe44d90f70e20f87a0ddc7250ffaf72
Bolun Zhao <bzhao325@gatech.edu>
2024-12-02 00:59:45 -0700

    Set layout and mock data

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Row, Col, Card } from "antd";
+import "./home.css";
+import { getData } from "../../api";
 
 const Home = () => {
-    return (
-        <div>
-            Home page
-        </div>
-    )
-}
+  const userImg = require("../../assets/images/spiderman.jpg");
+  useEffect(() => {
+    getData().then((res) => {
+        console.log(res, 'res')
+    })
+  }, [])
+  return (
+    <Row className="home">
+      <Col span={8}>
+        <Card hoverable>
+          <div className="user">
+            <img src={userImg} />
+            <div className="userinfo">
+              <p className="name">Admin</p>
+              <p className="access">Super Administrator</p>
+            </div>
+          </div>
+          <div className="login-info">
+            <p>
+              Last login time: <span>2024-9-19</span>
+            </p>
+            <p>
+              Last login location: <span>Seattle, WA</span>
+            </p>
+          </div>
+        </Card>
+      </Col>
+      <Col span={16}></Col>
+    </Row>
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,88 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu, theme } from "antd";
+import CommonSider from "../components/commonSider";
+import CommonHeader from "../components/commonHeader";
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+
+const { Header, Sider, Content } = Layout;
 
 const Main = () => {
-    return (
-        <div>
-            main
-            <Outlet />
-        </div>
-    )
-}
+  // const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  // Get the state of menu
+  const collapsed = useSelector((state) => state.tab.isCollapse);
+  return (
+    <Layout className="main-container">
+      {/* <Sider trigger={null} collapsible collapsed={collapsed}>
+          <h3 className='app-name'>Admin Portal System</h3>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: 'nav 1',
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2',
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3',
+              },
+            ]}
+            style={{
+                height: '100%'
+            }}
+          />
+        </Sider> */}
+      <CommonSider collapsed={collapsed} />
+      <Layout>
+        {/* <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          >
+            <Button
+              type="text"
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header> */}
+        <CommonHeader collapsed={collapsed} />
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
-export default Main
+export default Main;
